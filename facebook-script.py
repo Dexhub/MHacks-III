096b3df029781cc3c59a78a779b2f728fe82be42
Himanshu Shah <himanshu1410@live.com>
2014-01-18 00:37:42 -0500

    Checkpointing

--- facebook-script.py
+++ facebook-script.py
@@ -1,6 +1,6 @@
 #!/bin/python
 import facebook
-oauth_access_token = "CAACEdEose0cBAEV42kcZChyDjJS66jRojPECe2fdZAwojIKyNgVUYJlrwoQeEHIFUJ4qyHMjaFbYWLAsNMnRHOtV1MKRX8AWKu2gE78KH70ThY9dTzEN2jF6RY273e4DM3pbBZCl1ed4pRRW8wGnXPFR3C1L38ogXn16lOJ4xUwf7OBHZCfxJLD4LFbAvtWDBY45Pw2xTQZDZD"
+oauth_access_token = "CAACEdEose0cBAIWLdY15VT6yZBeBbQCo6o2MnaZCtU0Ql5Bo7fwiPev621KHRzmqxtliDGZAZAI1mTLYpBueI4ixibhT8xofmB4BZBBOtoCiYzKYjv2OA3G74RrMWrohAFfPYKkoXkGiDSJVb8EoTbprGcezSIklZCbHZAzZArxcd9kLtgDtDOXWcAoGVHEo7KYZAO25KRHokGwZDZD"
 
 graph = facebook.GraphAPI(oauth_access_token)
 #friendlistall = graph.get_object("me/friends(name,birthday)")
@@ -10,15 +10,23 @@ graph = facebook.GraphAPI(oauth_access_token)
 
 friends = graph.get_connections("me", "friends")
 
+friend_no = 5
 
 
 for friend in friends["data"]:
-  print "-"*10
-  print "Friend ID: ", friend["id"]
-  print "Name: ", friend["name"]
-  try:
-    print "Birthday", graph.get_object(friend["id"])["birthday"])
-  except:
-    print "No birthday"
+  if friend_no is not 0:
+    friend_no = friend_no -1
+    try:
+    #print "-"*10
+      print "Friend ID: ", friend["id"]
+      print "Name: ", friend["name"]
+      print "Birthday", graph.get_object(friend["id"])["birthday"]
+  #print request(self, friend["id"], args=birthday)
+    #print graph.get_object(friend["id"],birthday)
+    except:
+      print "No birthday"
   #print friend.get_birthday()
-  print "-"*10
+  else:
+    break
+
+print "-"*10
