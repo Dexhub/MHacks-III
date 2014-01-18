0c5663bc00ea87ea93ca05e68857f2057c38fa3b
Himanshu Shah <himanshu1410@live.com>
2014-01-17 22:06:07 -0500

    Gets birthdates just fine

--- facebook-script.py
+++ facebook-script.py
@@ -1,36 +1,24 @@
 #!/bin/python
 import facebook
-oauth_access_token = "CAACEdEose0cBAGD7M30FPP7NeZAVrpkLoNbx9focFdQ8O3sdQqJKFtCrACZAZB1d8GHLHHZBlhvwfftwoxNBAEUeBaawJZAMa8aqZBivYQJLtZAZBOVNqHVbbRlXCIP6eKmS4uZB94ClTZBIo0iRoJhppZAvK46b0Ljadv61F3IHHvyOaplP4kZCOefzQbTtlv4lCA3o0vGva8QSQAZDZD"
+oauth_access_token = "CAACEdEose0cBAEV42kcZChyDjJS66jRojPECe2fdZAwojIKyNgVUYJlrwoQeEHIFUJ4qyHMjaFbYWLAsNMnRHOtV1MKRX8AWKu2gE78KH70ThY9dTzEN2jF6RY273e4DM3pbBZCl1ed4pRRW8wGnXPFR3C1L38ogXn16lOJ4xUwf7OBHZCfxJLD4LFbAvtWDBY45Pw2xTQZDZD"
 
- 
 graph = facebook.GraphAPI(oauth_access_token)
-profile = graph.get_object("me")
-friends_data = graph.get_object("me/friends")
-print friends_data
-
-'''
-data = feed["data"]
-Thanks = ["Thanks a lot :)","Thanks a ton!!","Thanks for the wishes!!"]
-print "="*50
-i = 0
-print "="*50 
-for x in data:
-  i = i + 1
- # message = x["message"]
-  sender= (x["from"]["name"])
-#  try:
-#    sender = unicode(sender, "utf-8")
-#  except TypeError:
-#    sender = unicode(sender, "ascii")
-#  else:
-#    # value was valid ASCII data
-#    pass
+#friendlistall = graph.get_object("me/friends(name,birthday)")
+#friendlist = friendlistall["data"]
+#friendlist = graph.get_object("me/friends")
 
 
-#  value = unicode(sender, "utf-8")
-#  print sender
-#  print x["updated_time"] ,"=>",sender, "=>", message ,"=>", x["id"] ,"=>" ,Thanks[i%3]
- # Like the post and write comments
-  graph.put_like(x["id"])  
-  graph.put_comment(x["id"],Thanks[i%3])  
-print "="*50 '''
+friends = graph.get_connections("me", "friends")
+
+
+
+for friend in friends["data"]:
+  print "-"*10
+  print "Friend ID: ", friend["id"]
+  print "Name: ", friend["name"]
+  try:
+    print "Birthday", graph.get_object(friend["id"])["birthday"])
+  except:
+    print "No birthday"
+  #print friend.get_birthday()
+  print "-"*10
