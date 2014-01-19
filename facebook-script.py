1f6d82121f14b8d19d3da3694e7e40d5bb176274
Himanshu Shah <himanshu1410@live.com>
2014-01-18 21:58:57 -0500

    Checkpointing

--- facebook-script.py
+++ facebook-script.py
@@ -2,7 +2,10 @@
 import facebook
 import datetime,time
 import os
-oauth_access_token = "CAACEdEose0cBAPHeQHnJCbljjTufORGhyZAgWRgd3PUuOj4Aa8SpBWSQqPIJRnwwbPd5qvdlvbls9H29KXL0ZBGZALZBVShr5s1j3a0tTZAZATAhGZBZASp8u5FwyU0cEMqeuW96sQzf6MLQaXMGzXgrTnntkK6sBGgaXa77JmSytQJK7hRCRcIjOkCe0MCoIaIFeZCEzW8TIqgZDZD"
+import lob
+
+lob.api_key = "test_204515ce2c65d12b8ae9eea9f10f2edcba5"
+oauth_access_token = "CAACEdEose0cBAEBsaDIxzjrufURJ5HkWIkp5x3aKx6CLFxakbJTWg4myYNZBOCmZANkVKOcX4WXAI8MWpQNgbCMpLk85s48Xg2wLtZAvuvgLUStqygQqrNsgGBUnKrzsRO8a7iNg0CWxZCIIfB8gLjWZCZBVu16s4mBul3koBiyJARXnldaxtEoTheazL0fBcHewEcUi8A1QZDZD"
 #friendlistall = graph.get_object("me/friends(name,birthday)")
 #friendlist = friendlistall["data"]
 #friendlist = graph.get_object("me/friends")
@@ -96,7 +99,7 @@ def main():
     return CurrentDir
 
   def create_collage(person_name):
-    Dir = CurrentDir
+    #Dir = CurrentDir
     os.system("echo Run Collage Command Here!!") 
 
   def get_collage(graph,person_id, person_name, album_max, directory):
@@ -104,18 +107,97 @@ def main():
     get_data(graph, person_id, person_name, album_max, directory)
     create_collage(person_name)
 
+#  def what_type_object():
+#    print lob.Service.list()
+#    for service in lob.Service.list():
+#      dir(service)
+#      #print service
+#      print x[0].id
+#    print lob.setting.list() 
+  
+  def print_object(job_name, pdf_file, from_add, to_add, choice_id = 0, quantity = 1):
+    #print sender_name
+    #print from_add
+
+    object_created = lob.Object.create(name = job_name, file = pdf_file,
+    setting_id=lob.Setting.list()[0].id, quantity=1).to_dict()
+    last = len(lob.Object.list())
+    print "Job Created", lob.Job.create(name = job_name, to = to_add,
+                         objects=lob.Object.list()[last-1].id, from_address = from_add).to_dict()
+#    print "Object Created : ", object_created
+   
+#    print "Job Created: ", (lob.Job.create(sender_name, from_add,
+#                         object_created, from_address=from_add)).to_dict()
+
+   # print "Job Created", lob.Job.create(name = sender_name, to=lob.Address.list(count=1)[0].id,
+   #                      objects=lob.Object.list()[last-1].id, from_address=lob.Address.list(count=1,
+   #                                                                                     offset=5)[0].id).to_dict()
+
+  def send_postcard(job_name, front, back, from_add, to_add):
+    print "Post Card", lob.Postcard.create(name = job_name, to = to_add, front = front,
+                                                              back = back,
+                                                              from_address= from_add).to_dict()
+    
+    
+
 #================== Function Code =========================
-  friend_no = 10 # Number of friends
-  shipping_time = 5 # Days
-  album_max = 2 # Maximum number of Albums to be downloaded
-  graph  = authenticate()
-  friends_info = get_all_birthdays(graph, friend_no)
-  #print friends_info
-  birthday_people = within_range(friends_info, shipping_time)
-  directory = create_main_directory(graph)
-  for person in birthday_people:
-    print "Get Collage for %s" % person["name"]
-    get_collage(graph, person["id"], person["name"], album_max, directory)
+#  friend_no = 10 # Number of friends
+#  shipping_time = 5 # Days
+#  album_max = 2 # Maximum number of Albums to be downloaded
+#  graph  = authenticate()
+#  friends_info = get_all_birthdays(graph, friend_no)
+#  #print friends_info
+#  birthday_people = within_range(friends_info, shipping_time)
+#  directory = create_main_directory(graph)
+#  for person in birthday_people:
+#    print "Get Collage for %s" % person["name"]
+#    get_collage(graph, person["id"], person["name"], album_max, directory)
+#
+##################### Abhinav Tests ################
+#hk = User()
+####################################################
+
+    ################ after creating a post card 
+
+  ## Code for lob
+  from_addr = {
+            'name': 'Siddharth Saha',
+            'address_line1': '220 William T Morrissey',
+            'address_line2': 'Sunset Town',
+            'address_city': 'Boston',
+            'address_state': 'MA',
+            'address_country': 'US',
+            'address_zip': '02125'
+        }
+  to_addr = {
+            'name': 'Siddharth Saha',
+            'address_line1': '220 William T Morrissey',
+            'address_line2': 'Sunset Town',
+            'address_city': 'Boston',
+            'address_state': 'MA',
+            'address_country': 'US',
+            'address_zip': '02125'
+        }
+  name_job = 'Testing Print Object Post Card'
+  file_pdf = "http://www.cs.stonybrook.edu/~hkshah/PDF/CV.pdf"
+  #back_pdf = "PC_front.pdf"
+  back_pdf = front_pdf = "/home/dexter/scripts/temp/wishme-thankyou/my-test/Hackathon-MHacks/PC_front.pdf"
+  #front_pdf = "http://100startup.com/resources/business-plan.pdf"
+  #print_object(job_name = name_job, pdf_file = file_pdf, from_add = from_addr, to_add =  to_addr)
+  send_postcard(job_name= name_job, front = front_pdf, back = back_pdf, from_add = from_addr, to_add= to_addr)
+
+
+
+
+#  print lob.Object.list()
+  #print address.to_dict()
+  #print lob.Address.list()
+
+
+
+
+
+  #print lob.Postcard.list()
 
 
 if __name__ == "__main__":
